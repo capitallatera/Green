965081715b78cf6014a8699739d0641a1e603982
Anil S. H <capitallatera@gmail.com>
2023-09-18 15:29:38 +0530

    Started with Dashboard page

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import Dashboard from './Components/Dashboard'
+
+const App = () => {
+  return <Dashboard />
+}
+
+export default App
+
+const styles = StyleSheet.create({})
