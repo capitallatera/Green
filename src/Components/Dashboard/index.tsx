b93753dea6c24938d9d2c02d0ce78718717e9b02
Anil S. H <capitallatera@gmail.com>
2023-09-20 02:23:29 +0530

    Created Header component with static image

--- src/Components/Dashboard/index.tsx
+++ src/Components/Dashboard/index.tsx
@@ -1,14 +1,21 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { styles } from './styled'
 
 const Dashboard = () => {
   return (
-    <View>
-      <Text>Dashboard</Text>
+    <View style={styles.container}>
+      <View style={styles.head}>
+        <Text style={{
+          fontSize: 20
+        }}>Veges</Text>
+        <Image 
+          style={styles.image} 
+          source={require('../../assets/person_1.jpg')}
+        />
+      </View>
     </View>
   )
 }
 
 export default Dashboard
-
-const styles = StyleSheet.create({})
---
+++ src/Components/Dashboard/styled.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container : {
+        // borderWidth: 1,
+        // borderColor: 'red',
+        flex: 1,
+        margin : 10,
+    },
+    head : {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems : 'center'
+    },
+    image : {
+        width: 50,
+        height: 50,
+        borderRadius : 15
+    }
+})
